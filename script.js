9e278b15c0b9f089ab9875e60be30d934f5ced34
Samuel Odira <52766763+czsamzzy@users.noreply.github.com>
2020-07-25 20:32:57 +0100

    huplayer winning

--- script.js
+++ script.js
@@ -21,10 +21,41 @@ function startGame() {
   for (var i = 0; i < cells.length; i++) {
     cells[i].innerText = "";
     cells[i].style.removeProperty("background-color");
-    cells[i].addEventListener("click", turnClick, false); 
+    cells[i].addEventListener("click", turnClick, false);
   }
 }
 
 function turnClick(square) {
-    console.log(square.target.id)
+  turn(square.target.id, huPlayer);
+}
+
+function turn(squareId, player) {
+  origBoard[squareId] = player;
+  document.getElementById(squareId).innerText = player;
+  let gameWon = checkWin(origBoard, player);
+  if (gameWon) gameOver(gameWon);
+}
+
+function checkWin(board, player) {
+  let plays = board.reduce((a, e, i) => 
+   (e === player) ? a.concat(i) : a, []);
+  let gameWon = null;
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every((elem) => plays.indexOf(elem) > -1)) {
+      gameWon = { index: index, player: player };
+    }
+  }
+
+  return gameWon;
+}
+
+function gameOver(gameWon) {
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor =
+      gameWon.player == huPlayer ? "blue" : "red";
+  }
+
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener("click", turnClick, false);
+  }
 }
